8452273e316fc50e4e37f78baa813b1ac4d5e7b1
Swmpus <morganshipsides@outlook.com>
2020-02-29 13:39:49 +0000

    Added Airport.java

---
+++ Airport.java
@@ -0,0 +1,14 @@
+public class Airport
+{
+	private List<Runway> runways;
+
+	public Airpor(List<Runway> runways)
+	{
+		this.runways = runways;
+	}
+
+	public List<Runway> getRunways()
+	{
+		return runways;
+	}
+}
