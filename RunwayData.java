4cca4be2e9af62b2dc7f63605b768068231c27b0
Kareem <kareemalaa2001@gmail.com>
2020-02-29 14:04:47 +0000

    added a few comments

--- RunwayData.java
+++ RunwayData.java
@@ -1,10 +1,11 @@
 public class RunwayData
 {
-	public int threshold
-	public int TORA;
-	public int TODA;
-	public int ASDA;
-	public int LDA;
+	public int threshold; //  displaced threshold
+	public int TORA; //  take off run available
+	public int TODA; //  take off distance available
+	public int ASDA; //  accelerate-stop distance available
+	public int LDA;  //  landing distance available
+
 
 	public RunwayData(int threshold, int TORA, int TODA, int ASDA, int LDA)
 	{
