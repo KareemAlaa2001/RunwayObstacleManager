05760d932459dd00bce93e8cdd5a872c67f979d9
Swmpus <morganshipsides@outlook.com>
2020-03-11 16:52:36 +0000

    Implemented Threshold for Taking off From

--- Functions.java
+++ Functions.java
@@ -4,16 +4,18 @@ public class Functions
 	{ // Very dirty due to prints and overlapping repeat code, needs cleaning
 		int slopeAllowance = obstacle.maxHeight * Airport.MinSlope;
 
+		int newTakeoffThreshold = -1;
 		int newThreshold = -1;
 		int newTORA = -1;
 		int newTODA = -1;
 		int newASDA = -1;
 		int newLDA = -1;
 
-		boolean towards = obstacle.position > (runway.TORA / 2); // Need to fix this
+		boolean towards = obstacle.position > (runway.TORA / 2); // Not exactly perfect, find a way to do better
 
 		if (towards && slopeAllowance >= Airport.RESA) {
 			newThreshold = runway.threshold;
+			newTakeoffThreshold = 0;
 			newTORA = newTODA = newASDA = runway.threshold + obstacle.position - slopeAllowance - Airport.StripEnd;
 			newLDA = obstacle.position - Airport.RESA - Airport.StripEnd;
 
@@ -21,19 +23,23 @@ public class Functions
 			System.out.println("New LDA = " + obstacle.position + " - " + Airport.RESA + " - " + Airport.StripEnd + " = " + newLDA);
 		} else if (towards && slopeAllowance < Airport.RESA) {
 			newThreshold = runway.threshold;
+			newTakeoffThreshold = 0;
 			newTORA = newTODA = newASDA = runway.threshold + obstacle.position - Airport.RESA - Airport.StripEnd;
 			newLDA = obstacle.position - Airport.RESA - Airport.StripEnd;
 
 			System.out.println("New TORA, TODA, ASDA = " + runway.threshold + " + " + obstacle.position + " - " + slopeAllowance + " - " + Airport.StripEnd + " = " + newTORA);
 			System.out.println("New LDA = " + obstacle.position + " - " + Airport.RESA + " - " + Airport.StripEnd + " = " + newLDA);
 		} else if (Airport.BlastAllowance > (Airport.RESA + Airport.StripEnd)) {
-			newThreshold = runway.threshold + obstacle.position + Airport.BlastAllowance;
-			System.out.println("New Threshold = " + runway.threshold + " + " + obstacle.position + " + " + Airport.BlastAllowance + " = " + newThreshold);
+			newTakeoffThreshold = runway.threshold + obstacle.position + Airport.BlastAllowance;
 
 			if (Airport.RESA > slopeAllowance) {
+				newThreshold = runway.threshold + obstacle.position + Airport.RESA + Airport.StripEnd;
+				System.out.println("New Threshold = " + runway.threshold + " + " + obstacle.position + " + " + Airport.RESA + " + " + Airport.StripEnd + " = " + newThreshold);
 				newLDA = runway.LDA - obstacle.position - Airport.RESA - Airport.StripEnd;
 				System.out.println("New LDA = " + runway.LDA + " - " + obstacle.position + " - " + Airport.RESA + " - " + Airport.StripEnd + " = " + newLDA);
 			} else {
+				newThreshold = runway.threshold + obstacle.position + slopeAllowance + Airport.StripEnd;
+				System.out.println("New Threshold = " + runway.threshold + " + " + obstacle.position + " + " + slopeAllowance + " + " + Airport.StripEnd + " = " + newThreshold);
 				newLDA = runway.LDA - obstacle.position - slopeAllowance - Airport.StripEnd;
 				System.out.println("New LDA = " + runway.LDA + " - " + obstacle.position + " - " + slopeAllowance + " - " + Airport.StripEnd + " = " + newLDA);
 			}
@@ -45,13 +51,17 @@ public class Functions
 			System.out.println("New TODA = " + newTORA + " + " + runway.clearway + " = " + newTODA);
 			System.out.println("New ASDA = " + newTORA + " + " + runway.stopway + " = " + newASDA);
 		} else if (Airport.BlastAllowance <= (Airport.RESA + Airport.StripEnd)) {
-			newThreshold = runway.threshold + obstacle.position + Airport.RESA + Airport.StripEnd;
+			newTakeoffThreshold = runway.threshold + obstacle.position + Airport.RESA + Airport.StripEnd;
 			System.out.println("New Threshold = " + runway.threshold + " + " + obstacle.position + " + " + Airport.RESA + " + " + Airport.StripEnd + " = " + newThreshold);
 			
 			if (Airport.RESA > slopeAllowance) {
+				newThreshold = runway.threshold + obstacle.position + Airport.RESA + Airport.StripEnd;
+				System.out.println("New Threshold = " + runway.threshold + " + " + obstacle.position + " + " + Airport.RESA + " + " + Airport.StripEnd + " = " + newThreshold);
 				newLDA = runway.LDA - obstacle.position - Airport.RESA - Airport.StripEnd;
 				System.out.println("New LDA = " + runway.LDA + " - " + obstacle.position + " - " + Airport.RESA + " - " + Airport.StripEnd + " = " + newLDA);
 			} else {
+				newThreshold = runway.threshold + obstacle.position + slopeAllowance + Airport.StripEnd;
+				System.out.println("New Threshold = " + runway.threshold + " + " + obstacle.position + " + " + slopeAllowance + " + " + Airport.StripEnd + " = " + newThreshold);
 				newLDA = runway.LDA - obstacle.position - slopeAllowance - Airport.StripEnd;
 				System.out.println("New LDA = " + runway.LDA + " - " + obstacle.position + " - " + slopeAllowance + " - " + Airport.StripEnd + " = " + newLDA);
 			}
--- RunwayData.java
+++ RunwayData.java
@@ -1,16 +1,18 @@
 public class RunwayData
 {
-	public int threshold;	// displaced threshold after recalculation (could be the same value)
+	public int threshold;	// Displaced threshold after recalculation (could be the same value)
+	public int takeoffThreshold; // Similar to above but for the start of the takeoff run
 	public int stopway;
 	public int clearway;
-	public int TORA;		// take off run available
-	public int TODA;		// take off distance available
-	public int ASDA;		// accelerate-stop distance available
-	public int LDA;			// landing distance available
+	public int TORA;		// Take Off Run Available
+	public int TODA;		// Take Off Distance Available
+	public int ASDA;		// Accelerate-Stop Distance Available
+	public int LDA;			// Landing Distance Available
 
 	public RunwayData(int threshold, int TORA, int stopway, int clearway)
 	{ // clearway totally seperate to stopway, call this function when initialising a runway
 		this.threshold = threshold;
+		this.takeoffThreshold = 0;
 		this.TORA = TORA;
 		this.ASDA = TORA + stopway;
 		this.TODA = TORA + clearway;
@@ -22,6 +24,7 @@ public class RunwayData
 	public RunwayData(int threshold, int TORA, int TODA, int ASDA, int LDA)
 	{ // clearway totally seperate to stopway, call this function when initialising a runway
 		this.threshold = threshold;
+		this.takeoffThreshold = 0;
 		this.TORA = TORA;
 		this.ASDA = ASDA;
 		this.TODA = TODA;
@@ -30,9 +33,10 @@ public class RunwayData
 		this.clearway = TODA - TORA;
 	}
 
-	public RunwayData(int threshold, int stopway, int clearway, int TORA, int ASDA, int TODA, int LDA)
+	public RunwayData(int threshold, int takeoffThreshold, int stopway, int clearway, int TORA, int ASDA, int TODA, int LDA)
 	{ // Any usage of this constructor should be marked as such, only called in recalculation
 		this.threshold = threshold;
+		this.takeoffThreshold = takeoffThreshold;
 		this.TORA = TORA;
 		this.ASDA = ASDA;
 		this.TODA = TODA;
