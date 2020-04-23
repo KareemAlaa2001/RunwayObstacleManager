12b6feba968fea8325968f6841c56a821b844c41
Swmpus <morganshipsides@outlook.com>
2020-04-23 23:02:58 +0100

    Implemented InputScreenController

--- InputScreenController.java
+++ InputScreenController.java
@@ -1,8 +1,25 @@
-import java.util.ArrayList;
 import java.util.List;
-import javafx.application.Application;
-import javafx.stage.Stage;
 
 public class InputScreenController
 {
+	public static Airport initialiseAirport(int RESA, int StripEnd, int BlastAllowance, int MinSlope)
+	{
+		return new Airport(RESA, StripEnd, BlastAllowance, MinSlope);
+	}
+
+	public static Runway initRunway(int bearing, RunwayData left, RunwayData right)
+	{
+		return new Runway(bearing, left, right);
+	}
+
+	public static RunwayData initRunwayData(int threshold, int TORA, int stopway, int clearway)
+	{
+		return new RunwayData(threshold, TORA, stopway, clearway);
+	}
+
+	public static Airport addRunwaysToAirport(Airport airport, List<Runway> runways)
+	{
+		airport.addRunways(runways);
+		return airport;
+	}
 }
